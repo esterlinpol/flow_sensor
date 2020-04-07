f5919ddf031eaf16d0615a2b7340b19b96d640bf
Esterlin Polanco <Esterlinpol@gmail.com>
2020-04-07 17:05:38 -0400

    EOD
    -Replaced serial for i2c.
    -Changed multiplier to 100.

--- flow_sensor.ino
+++ flow_sensor.ino
@@ -7,11 +7,14 @@
   ------------------------------------------------------------------------------
   Description: 
   Code for Water flow metter system, built for physics 2 class at Dominico Americano
-  University, Santo Domingo D.R. Connects to ESP8266 via serial. 
+  University, Santo Domingo D.R. Connects to ESP8266 via i2c. 
   ------------------------------------------------------------------------------
   License:
   Please see attached LICENSE.txt file for details.
 ------------------------------------------------------------------------------*/
+#include <Wire.h>
+
+//Flow Sensor variables:
 int sendvalue;
 const int sensorPin = 2;
 const int measureInterval = 1000;
@@ -19,42 +22,69 @@ volatile int pulseConter;
 const float factorK = 7.5;
 float frequency;
 float flow_Lmin;
- 
-/*------------------------------------------------------------------------------
-  Flow Sensor methods:
-------------------------------------------------------------------------------*/ 
-void ISRCountPulse()
- {
-   pulseConter++;
- }
- 
-float GetFrequency()
- {
-   pulseConter = 0;
- 
-   interrupts();
-   delay(measureInterval);
-   noInterrupts();
- 
-   return (float)pulseConter * 1000 / measureInterval;
- }
 
-void measure()
- {
-   frequency = GetFrequency(); 
-   flow_Lmin = frequency / factorK;
-   //Multiply to make the value suitable for integer to be sent:
-   sendvalue = flow_Lmin * 1000;
- } 
-void setup() {
-  // Begin the Serial at 9600 Baud
-  Serial.begin(115200);
+//i2c Address:
+const byte SlaveDeviceId = 1;
+
+/*------------------------------------------------------------------------------
+  Flow sensor methods:
+------------------------------------------------------------------------------*/ 
+  void ISRCountPulse()
+  {
+    pulseConter++;
+  }
+  
+  float GetFrequency()
+  {
+    pulseConter = 0;
+  
+    interrupts();
+    delay(measureInterval);
+    noInterrupts();
+  
+    return (float)pulseConter * 1000 / measureInterval;
+  }
+
+  int measure()
+  {
+    frequency = GetFrequency(); 
+    flow_Lmin = frequency / factorK;
+    
+    //Multiply to make the value suitable for integer to be sent:
+    sendvalue = flow_Lmin * 100;
+    return sendvalue;
+
+  }
+
+/*------------------------------------------------------------------------------
+  I2C methods:
+------------------------------------------------------------------------------*/ 
+  void requestCallback()
+  {
+    int input = sendvalue;
+    
+    // Send two bytes to master.
+    uint8_t buffer[2];
+    buffer[0] = input >> 8;
+    buffer[1] = input & 0xff;  
+    Wire.write(buffer, 2);
+  }
+
+void setup()
+{
+  // Start I2C bus as a slave
+  Wire.begin(SlaveDeviceId); 
+  // Set the callback to call when data is requested. 
+  Wire.onRequest(requestCallback);
+
+  //interrupts initialization:
   attachInterrupt(digitalPinToInterrupt(sensorPin), ISRCountPulse, RISING);
+}
+
+void loop()
+{
+  //Calculate the water flow and stores the value to send over i2c:
+  sendvalue = measure();
   
 }
 
-void loop() {
-  measure();
-  Serial.print(sendvalue); //Write the serial data
-  delay(400);
-}
